6ccb32731d2d501db45693965f5a2cfc3b8f96f9
ZacchNaa <2016.naaz@gmail.com>
2021-06-06 08:16:33 +0000

    Add further checks to bmiCalculator() and, imojies

--- js/script.js
+++ js/script.js
@@ -18,7 +18,8 @@ let output;
 // FUNCTIONS****************************************
 
 //function returns boolean for invalid inputs
-const isNotValidInput = (value) => (value <= 0 || isNaN(value) ? true : false);
+const isNotValidInput = (value) =>
+  value <= 0 || isNaN(value) || value === "" ? true : false;
 
 //BMI Calculation function
 const bmiCalculator = (weight, height) =>
@@ -28,15 +29,15 @@ const bmiCalculator = (weight, height) =>
 const checkBMI = (bmiValue) => {
   //BMI Message to display
   if (bmiValue < 18.5) {
-    output = `Your BMI is ${bmiValue}kg/m<sup>2</sup> , so you are underweight.`;
+    output = `Your BMI is ${bmiValue}kg/m<sup>2</sup> , so you are underweight. 😮`;
     message.classList.add("danger"); //change text color to red
     message.classList.remove("normal"); //remove normal class
   } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
-    output = `Your BMI is ${bmiValue}kg/m<sup>2</sup> , so you have a normal weight.`;
+    output = `Your BMI is ${bmiValue}kg/m<sup>2</sup> , so you have a normal weight. 🙂`;
     message.classList.add("normal"); //add normal class
     message.classList.remove("danger"); //remove danger class
   } else {
-    output = `Your BMI is ${bmiValue}kg/m<sup>2</sup> , so you are overweight.`;
+    output = `Your BMI is ${bmiValue}kg/m<sup>2</sup> , so you are overweight. 😮`;
     message.classList.remove("normal"); //remove normal class
     message.classList.add("danger"); //change text color to red
   }
@@ -47,13 +48,20 @@ const bmiDisplayMessage = (e) => {
   e.preventDefault(); //prevents form default action
 
   //call to bmiCalculator function
-  const bmi = bmiCalculator(parseFloat(weight.value), parseFloat(height.value));
+  if (!(isNotValidInput(weight.value) || isNotValidInput(height.value))) {
+    const bmi = bmiCalculator(
+      parseFloat(weight.value),
+      parseFloat(height.value)
+    );
 
-  // check BMI and display the correct message
-  checkBMI(bmi);
+    // check BMI and display the correct message
+    checkBMI(bmi);
 
-  // function return
-  return (message.innerHTML = output);
+    // function return
+    return (message.innerHTML = output);
+  } else {
+    return (message.innerHTML = `Please enter correct values. 🤦‍♂️`);
+  }
 };
 
 //LISTENERS*******************************************
@@ -63,7 +71,7 @@ weight.addEventListener(`keyup`, () => {
   if (isNotValidInput(weight.value)) {
     weight.classList.add("error"); //add red border to the input field
     message.classList.add("danger"); //change text color to red
-    message.innerHTML = `Please enter weight in kilograms (kg).`;
+    message.innerHTML = `Please enter weight in kilograms (kg). 🙄`;
   } else {
     weight.classList.remove("error");
     message.classList.remove("danger");
@@ -76,7 +84,7 @@ height.addEventListener(`keyup`, () => {
   if (isNotValidInput(height.value)) {
     height.classList.add("error"); //add red border to the input field
     message.classList.add("danger"); //change text color to red
-    message.innerHTML = `Please enter height in meters (m).`;
+    message.innerHTML = `Please enter height in meters (m). 🙄`;
   } else {
     height.classList.remove("error");
     message.classList.remove("danger");
